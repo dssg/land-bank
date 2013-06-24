27c121d3ab579ae99d5be7ced5540ab31662e835
skyinthecloud <swhorton@ip-10-253-33-241.us-west-2.compute.internal>
2013-06-24 15:12:54 +0000

    Generalized the load auctions script and factored out SPSS timestamp conversion function

--- landbank/landbank/settings.py
+++ landbank/landbank/settings.py
@@ -1,3 +1,7 @@
+import os
+import sys
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, "../landbank_site"))
 # Django settings for landbank project.
 
 DEBUG = True
