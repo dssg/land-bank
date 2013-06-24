06569c745d4b669f6cc0bcfdad9aa19ef8dfe3d4
skyinthecloud <swhorton@ip-10-253-33-241.us-west-2.compute.internal>
2013-06-24 15:12:54 +0000

    Generalized the load auctions script and factored out SPSS timestamp conversion function

--- landbank/landbank_site/models.py
+++ landbank/landbank_site/models.py
@@ -38,36 +38,5 @@ class Auction(models.Model):                                         # results o
   def __unicode__(self):
     return unicode(self.pin)
 
-# class PoliceBeat(models.Model):
-#   district = models.IntegerField('District')
-#   sector   = models.IntegerField('Sector')
-#   beat     = models.IntegerField('Beat')
-#   beat_num = models.IntegerField('Beat number')
-
-#   mpoly    = models.MultiPolygonField()
-#   objects  = models.GeoManager()
-
-#   def __unicode__(self):
-#     return self.beat_num
-
-## class Crime(models.Model):
-#   crimeid   = models.CharField('ID', max_length=20)
-#   caseno    = models.CharField('Case Number', max_length=20)
-#   timestamp = models.DateTimeField('Timestamp')
-#   block     = models.CharField('Block', max_length=60)
-#   iucr      = models.CharField('IUCR',max_length=10)
-#   ptype     = models.CharField('Primary Type', max_length=100)
-#   desc      = models.CharField('Description', max_length=100)
-#   locdesc   = models.CharField('Location Description', max_length=100)
-#   arrest    = models.BooleanField('Arrest')
-#   domestic  = models.BooleanField('Domestic')
-#   beat      = models.IntegerField('Beat number')
-#   district  = models.IntegerField('District',null=True)
-#   ward      = models.IntegerField('Ward',null=True)
-#   comm_area = models.IntegerField('Community Area',null=True)
-  
-#   loc       = models.PointField(null=True)
-#   objects   = models.GeoManager()
-
-#   def __unicode__(self):
-#     return self.crimeid
+  class Meta:
+    app_label = 'landbank_site'
