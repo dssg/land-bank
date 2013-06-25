013838df35ba7a782ae8cc1d695dea6180d4abc3
skyinthecloud <swhorton@ip-10-253-33-241.us-west-2.compute.internal>
2013-06-25 14:49:08 +0000

    Installed South 0.8.1, added initial migration for five IHS database tables

--- landbank/landbank/settings.py
+++ landbank/landbank/settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'south',
     'landbank_site',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
