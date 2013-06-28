54d4d77ee0b84fa21191eb428769c2baa67e0efa
Skyler Whorton <skyler.whorton@gmail.com>
2013-06-28 16:15:12 +0000

    Installed django-extensions and pygraphviz in order to generate Django model diagrams automatically. Added first diagram. Partially addresses #19.

--- landbank/landbank/settings.py
+++ landbank/landbank/settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'django_extensions',
     'south',
     'tastypie',
     'landbank_data',
