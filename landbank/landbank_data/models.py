309f40f2b0ee4826816236bae825f484ef3109fe
Skyler Whorton <skyler.whorton@gmail.com>
2013-06-28 22:00:23 +0000

    Added 'Scavenger' as a django model; created and applied database migration; generated new db model image; created and tested load script; ran the import successfully. Reproduced Katie's summary results successfully. Closes #18.

--- landbank/landbank_data/models.py
+++ landbank/landbank_data/models.py
@@ -242,3 +242,17 @@ class Assessor(models.Model):
   class Meta:
     app_label = 'landbank_data'
 
+
+class Scavenger(models.Model):
+  pin                           = models.CharField('Property ID number', max_length=14, db_index=True)
+  township			= models.IntegerField('Township ID number', null=True)
+  volume			= models.CharField('???', max_length=3, null=True)
+  tax_year			= models.IntegerField('Year of tax balance', null=True)
+  tax_type			= models.CharField('???', max_length=2, null=True)
+  tax_amount			= models.FloatField('???', null=True)
+  total_amount			= models.FloatField('???', null=True)
+  def __unicode__(self):
+    return unicode(self.pin)
+  class Meta:
+    app_label = 'landbank_data'
+
