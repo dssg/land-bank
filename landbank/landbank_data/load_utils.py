71123bfa474d6691facfd8c13014ceb4dcf9f834
Skyler Whorton <skyler.whorton@gmail.com>
2013-06-28 22:00:23 +0000

    Added 'Scavenger' as a django model; created and applied database migration; generated new db model image; created and tested load script; ran the import successfully. Reproduced Katie's summary results successfully. Closes #18.

--- landbank/landbank_data/load_utils.py
+++ landbank/landbank_data/load_utils.py
@@ -8,9 +8,17 @@ utc = pytz.utc
 central_tz = timezone('US/Central')
 gregorian_diff = (datetime.datetime(1970,1,1) - datetime.datetime(1582,10,14)).total_seconds()
 year_2100 = time.mktime(datetime.datetime(2100,1,1).timetuple())
-
 def spss_to_posix(spss_timestamp):
 	if (spss_timestamp > year_2100):
 		return datetime.datetime.fromtimestamp(Decimal(spss_timestamp)-Decimal(gregorian_diff)).replace(tzinfo=utc).astimezone(central_tz) 
 	else:
 		return spss_timestamp
+
+# Converts a string in the form '08' and returns a 4-digit integer year e.g. 2008
+this_year = datetime.datetime.now().year
+def yy_to_yyyy(yy):
+	yy_num = int(yy)
+	if (this_year-2000 >= yy_num):
+		return 2000 + yy_num
+	else:
+		return 1900 + yy_num 
