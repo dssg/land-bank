e997bf8ad79f9957790a5271e33a5e5e90cb9b27
Skyler Whorton <skyler.whorton@gmail.com>
2013-07-02 21:57:47 +0000

    Minor fixes but major code refactor to import scripts, database schemas. Restarted migration history at 0001. Closes #21.

--- landbank/landbank_data/load_cashfin.py
+++ landbank/landbank_data/load_cashfin.py
@@ -17,71 +17,109 @@ def load_cashfin(cashfin_file, verbose = False):
     for row in reader:
       #if (i==10):
         #break
+      pin = '{:0>14}'.format(int(Decimal(row[1])))
+      doc = row[2]
+      date_doc = spss_to_posix(row[3])
+      date_rec = spss_to_posix(row[4])
+      try:	year = int(row[5])
+      except:	year = None
+      try: 	amount_prime = float(row[6])
+      except:   amount_prime = None
+      likely_distressed  = True if int(row[7])==1 else False
+      likely_cash = True if int(row[8])==1 else False
+      buyer = row[9].strip()
+      buyer_type = row[10].strip()
+      seller = row[11].strip()
+      seller_type = row[12].strip()
+      apt = row[13].strip()
+      direction = row[14].strip()
+      houseno = row[15].strip()
+      street = row[16].strip()
+      suffix = row[17].strip()
+      addr_final = row[18].strip()
+      city_final = row[19].strip()
+      try:	lat_y = float(row[20])
+      except:	lat_y = None
+      try:	long_x = float(row[21])
+      except:	long_x = None
+      try:	tract_fix = float(row[22])
+      except:	tract_fix = None
+      no_tract_info = True if int(row[23])==1 else False
+      try:	ca_num = int(row[24])
+      except:	ca_num = None
+      ca_name = row[25].strip()
+      place = row[26].strip()
+      gisdate = row[27].strip()
+      try: 	ptype = int(row[28])
+      except:   ptype = None
+      try:	residential = int(row[29])
+      except:	residential = None
+      loc       = None if row[20]=='' else Point((Decimal(row[21]), Decimal(row[20])))
       try:
         cashfin =  CashFin.objects.get(\
-        pin     = '{:0>14}'.format(int(Decimal(row[1])))\
-        ,doc    = row[2]\
-        ,date_doc = spss_to_posix(row[3])\
-	,date_rec = spss_to_posix(row[4])\
-	,year = row[5]\
-	,amount_prime = row[6]\
-	,likely_distressed  = True if row[7]==1 else False\
-	,likely_cash = True if row[8]==1 else False\
-	,buyer = row[9].strip()\
-	,buyer_type = row[10].strip()\
-	,seller = row[11].strip()\
-	,seller_type = row[12].strip()\
-	,apt = row[13].strip()\
-	,direction = row[14].strip()\
-	,houseno = row[15].strip()\
-	,street = row[16].strip()\
-	,suffix = row[17].strip()\
-	,addr_final = row[18].strip()\
-	,city_final = row[19].strip()\
-	,lat_y = row[20]\
-	,long_x = row[21]\
-	,tract_fix = row[22]\
-	,no_tract_info = row[23]\
-	,ca_num = row [24]\
-	,ca_name = row[25].strip()\
-	,place = row[26].strip()\
-	,gisdate = row[27].strip()\
-	,ptype = row[28]\
-	,residential = row[29]\
-        ,loc       = None if row[20]=='' else Point((Decimal(row[21]), Decimal(row[20])))\
+        pin = pin\
+        ,doc = doc\
+        ,date_doc = date_doc\
+	,date_rec = date_rec\
+	,year = year\
+	,amount_prime = amount_prime\
+	,likely_distressed  = likely_distressed\
+	,likely_cash = likely_cash\
+	,buyer = buyer\
+	,buyer_type = buyer_type\
+	,seller = seller\
+	,seller_type = seller_type\
+	,apt = apt\
+	,direction = direction\
+	,houseno = houseno\
+	,street = street\
+	,suffix = suffix\
+	,addr_final = addr_final\
+	,city_final = city_final\
+	,lat_y = lat_y\
+	,long_x = long_x\
+	,tract_fix = tract_fix\
+	,no_tract_info = no_tract_info\
+	,ca_num = ca_num\
+	,ca_name = ca_name\
+	,place = place\
+	,gisdate = gisdate\
+	,ptype = ptype\
+	,residential = residential\
+        ,loc = loc\
         )
       except:
         cashfin =  CashFin(\
-        pin     = '{:0>14}'.format(int(Decimal(row[1])))\
-        ,doc    = row[2]\
-        ,date_doc = spss_to_posix(row[3])\
-	,date_rec = spss_to_posix(row[4])\
-	,year = row[5]\
-	,amount_prime = row[6]\
-	,likely_distressed  = True if row[7]==1 else False\
-	,likely_cash = True if row[8]==1 else False\
-	,buyer = row[9].strip()\
-	,buyer_type = row[10].strip()\
-	,seller = row[11].strip()\
-	,seller_type = row[12].strip()\
-	,apt = row[13].strip()\
-	,direction = row[14].strip()\
-	,houseno = row[15].strip()\
-	,street = row[16].strip()\
-	,suffix = row[17].strip()\
-	,addr_final = row[18].strip()\
-	,city_final = row[19].strip()\
-	,lat_y = row[20]\
-	,long_x = row[21]\
-	,tract_fix = row[22]\
-	,no_tract_info = row[23]\
-	,ca_num = row [24]\
-	,ca_name = row[25].strip()\
-	,place = row[26].strip()\
-	,gisdate = row[27].strip()\
-	,ptype = row[28]\
-	,residential = row[29]\
-        ,loc       = None if row[20]=='' else Point((Decimal(row[21]), Decimal(row[20])))\
+        pin = pin\
+        ,doc = doc\
+        ,date_doc = date_doc\
+	,date_rec = date_rec\
+	,year = year\
+	,amount_prime = amount_prime\
+	,likely_distressed  = likely_distressed\
+	,likely_cash = likely_cash\
+	,buyer = buyer\
+	,buyer_type = buyer_type\
+	,seller = seller\
+	,seller_type = seller_type\
+	,apt = apt\
+	,direction = direction\
+	,houseno = houseno\
+	,street = street\
+	,suffix = suffix\
+	,addr_final = addr_final\
+	,city_final = city_final\
+	,lat_y = lat_y\
+	,long_x = long_x\
+	,tract_fix = tract_fix\
+	,no_tract_info = no_tract_info\
+	,ca_num = ca_num\
+	,ca_name = ca_name\
+	,place = place\
+	,gisdate = gisdate\
+	,ptype = ptype\
+	,residential = residential\
+        ,loc = loc\
         )
       #i += 1
       cashfin.save()
