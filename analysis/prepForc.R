14238dd6f51cc6f8ac89095c0e905aeafea275f3
evan <evanmisshula@gmail.com>
2013-07-10 15:27:32 -0400

    additional plots and csv for Sophia

--- analysis/prepForc.R
+++ analysis/prepForc.R
@@ -2,10 +2,6 @@
 d <- read.table(file="~/Documents/chicago/dssg-landbank-project/analysis/testfc.csv",header=FALSE,sep=",",)
 library("stringr")
 library("zoo")
-=======
-d <- read.table(file="~/dssg-landbank-project/analysis/testfc.csv",header=FALSE,sep=",",)
-library(stringr)
-library(zoo)
 createString <- function(x){
   createString <- paste(substr(as.character(x),1,4),substr(as.character(x),5,5),sep="-")
   
@@ -15,12 +11,14 @@ createString <- function(x){
 community_area <- str_trim(d[,1],side = "both")
 d<-data.frame(d[,2],community_area,d[,3])
 colnames(d)[c(1,3)] <- c("quarterly num","foreclosure")
+head(d)
 dateStrings <- unlist(lapply(d[,1],createString))
 yyyyq_doc <- as.yearqtr(dateStrings)
 
- ## yyyyq_doc_date=as.Date(yyyyq_doc)
+yyyyq_doc_date=as.Date(yyyyq_doc)
 
-## d <- data.frame(d[,1],yyyyq_doc_date,d[,2:3])
+d <- data.frame(d[,1],yyyyq_doc_date,d[,2:3])
+head(d)
 ## =======
 ## d <- data.frame(d[,1],yyyyq_doc,d[,2:3])
 ## yyyyq_doc_date <- as.Date(yyyyq_doc)
