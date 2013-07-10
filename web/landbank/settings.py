b9e2614be1914397dc638a081512493b6f27a23c
Skyler Whorton <skyler.whorton@gmail.com>
2013-07-10 13:49:25 +0000

    Fixed settings to refer to new 'web' project name

--- web/landbank/settings.py
+++ web/landbank/settings.py
@@ -107,10 +107,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'landbank.urls'
+ROOT_URLCONF = 'web.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'landbank.wsgi.application'
+WSGI_APPLICATION = 'web.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
