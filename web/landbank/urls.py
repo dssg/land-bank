2cddd7554b9152adf640e2cf4b7ab42761cc0b89
Skyler Whorton <skyler.whorton@gmail.com>
2013-07-12 16:06:51 +0000

    Standing up initial API with a 'parcel' resource; reversing mistaken settings renaming I did in response to changing 'landbank' project to 'web'

--- web/landbank/urls.py
+++ web/landbank/urls.py
@@ -1,8 +1,14 @@
-from django.conf.urls import patterns, include, url
+#from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
+from tastypie.api import Api
+from landbank_data.api import ParcelResource
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+
+v1_api = Api(api_name='v1')
+v1_api.register(ParcelResource())
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,4 +20,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    (r'api/', include(v1_api.urls)),
 )
