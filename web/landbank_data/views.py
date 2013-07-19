f16d6a9e3f80b337a463288515a87154315493cb
Skyler Whorton <skyler.whorton@gmail.com>
2013-07-19 14:15:33 +0000

    Added more to PIN/parcel detail view, leaving out a search form for now, so closes #40

--- web/landbank_data/views.py
+++ web/landbank_data/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from landbank_data.models import Assessor
+from landbank_data.models import Assessor, PinAreaLookup, CommunityAreas, CensusTract, Wards
 
 def pin(request, search_pin=None):
 #    search_assessor = get_object_or_404(Assessor, pin=search_pin)
@@ -7,5 +7,21 @@ def pin(request, search_pin=None):
         search_assessor = Assessor.objects.get(pin=search_pin)
     except Assessor.DoesNotExist:
         search_assessor = None
-    return render(request, 'landbank_data/pin.html', {'assessor': search_assessor})
+    else:
+        lookup = PinAreaLookup.objects.get(pin=search_pin)
+
+    try:
+        ward = Wards.objects.get(pk=lookup.ward_id)
+    except:
+        ward = None
+    try:
+        ca = CommunityAreas.objects.get(pk=lookup.community_area_id)
+    except:
+        ca = None
+    try:
+        tract = CensusTract.objects.get(pk=lookup.census_tract_id)
+    except:
+        tract = None
+
+    return render(request, 'landbank_data/pin.html', {'assessor': search_assessor, 'ward': ward, 'ca': ca, 'tract': tract})
 
