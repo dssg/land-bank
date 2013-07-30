b169780c0c2fd05b950de475f6df43edb18c1ab2
Skyler Whorton <skyler.whorton@gmail.com>
2013-07-30 21:26:01 +0000

    Fixed bug in PinAreaLookup with mismatched field names and outdated Assessor.ptype_desc field name in pin.html template

--- web/landbank_data/models.py
+++ web/landbank_data/models.py
@@ -476,9 +476,9 @@ class CommunityAreas(models.Model):
 
 class PinAreaLookup(models.Model):
   pin				= models.CharField('Property ID number', max_length=15, null=False, db_index=True)
-  ward_id			= models.ForeignKey('Wards',null=True)
-  community_area_id		= models.ForeignKey('CommunityAreas',null=True)
-  census_tract_id		= models.ForeignKey('CensusTract',null=True)
+  ward				= models.ForeignKey('Wards',null=True)
+  community_area		= models.ForeignKey('CommunityAreas',null=True)
+  census_tract			= models.ForeignKey('CensusTract',null=True)
   def __unicode__(self):
     return unicode(self.pin)
   class Meta:
