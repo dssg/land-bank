1b04049852a3b5370ee3bff36ba52dc831633664
Skyler Whorton <skyler.whorton@gmail.com>
2013-07-30 21:38:10 +0000

    Added 'median' method for calculating median of a field in a Django queryset

--- web/landbank/settings.py
+++ web/landbank/settings.py
@@ -3,6 +3,7 @@ import sys
 PROJECT_ROOT = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(PROJECT_ROOT, "../landbank_data"))
 sys.path.insert(0, os.path.join(PROJECT_ROOT, "../landbank_data/file_import"))
+sys.path.insert(0, os.path.join(PROJECT_ROOT, "../landbank_data/analysis"))
 # Django settings for landbank project.
 
 DEBUG = True
