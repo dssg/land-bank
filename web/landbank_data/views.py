b5ef63ef88ad7143ad5768a4170966ed8305f90a
tplagge <tplagge@ip-10-253-33-241.us-west-2.compute.internal>
2013-07-31 16:39:48 +0000

    D3 plot for community areas

--- web/landbank_data/views.py
+++ web/landbank_data/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404, render_to_response
+from django.template import RequestContext
 from landbank_data.models import Assessor, PinAreaLookup, CommunityAreas, CensusTract, Wards, Transaction
 import datetime
 import numpy as np
 from pytz import timezone
+import json
 
 def pin(request, search_pin=None):
 #    search_assessor = get_object_or_404(Assessor, pin=search_pin)
@@ -31,34 +33,24 @@ def pin(request, search_pin=None):
 def commarea(request, search_commarea=None):
   cas = [i.area_number for i in CommunityAreas.objects.all()]
   sfhs = []
-  condos = []
   cst = timezone('US/Central')
   oneyearago = cst.localize(datetime.datetime.now() - \
                             datetime.timedelta(days=365))
 
-  for ica, ca in enumerate(cas):
-    # Only do the first ten because it's slow!
-    if ica==10: break
-    sfhs.append(np.median([i.amount_prime for i in \
-      Transaction.objects.filter(ca_num__exact=ca).\
-      filter(date_doc__lte=oneyearago).filter(ptype=1)]))
-    condos.append(np.median([i.amount_prime for i in \
-      Transaction.objects.filter(ca_num__exact=ca).\
-      filter(date_doc__lte=oneyearago).filter(ptype=2)]))
-  mybins = np.arange(0,1e6+1,1e5)
-  sfh_vals, bins = np.histogram(sfhs,bins=mybins)
-  condo_vals, junk = np.histogram(condos,bins=mybins)
+  #for ica, ca in enumerate(cas):
+  #  # Only do the first ten because it's slow!
+  #  if ica==10: break
+  #  sfhs.append(np.median([i.amount_prime for i in \
+  #    Transaction.objects.filter(ca_num__exact=ca).\
+  #    filter(date_doc__lte=oneyearago).filter(ptype=1)]))
+  bins = [100,200,300,400,500]
+  #sfh_vals, bins = np.histogram(sfhs,bins=mybins)
+  sfh_vals = [1,2,3,2,1]
+  data = []
+  for b,v in zip(bins,sfh_vals):
+    data.append({'x':b, 'y':v})
 
-  chartdata = {'x':  bins[:-1], \
-               'y1': [float(i) for i in sfh_vals], \
-               'y2': [float(i) for i in condo_vals], \
-               'name1': 'Single family', \
-               'name2': 'Condos'}
-  charttype = "multiBarChart"
-  data = {
-    'charttype': charttype,
-    'chartdata': chartdata
-  }
   print data
-  return render_to_response('commarea.html', data)
+  return render_to_response('commarea2.html', {'data': json.dumps(data)},\
+                            context_instance=RequestContext(request))
 
