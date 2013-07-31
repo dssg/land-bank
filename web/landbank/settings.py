642e309ee2ef41cf72408ba3663df5b774fdcf3b
Evan Misshula <emisshula@jjay.cuny.edu>
2013-07-31 16:09:03 +0000

    changed the models to include pandas_django and included upstream changes

--- web/landbank/settings.py
+++ web/landbank/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'django_extensions',
+    'django_pandas',
     'south',
     'tastypie',
     'landbank_data',
