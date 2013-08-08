be957056cef561443b59797f31c1ed2ae9105a61
Skyler Whorton <skyler.whorton@gmail.com>
2013-08-08 14:26:29 +0000

    Added census tract characteristics model and added vacancy score to census tract scores model.

--- web/landbank_data/models.py
+++ web/landbank_data/models.py
@@ -584,6 +584,7 @@ class TractScores(models.Model):
   nuisance	= models.IntegerField('Nuisance score from 311, crime, etc.', null=True)
   affordability = models.IntegerField('Percent of tract population estimated to be able to afford median-priced SFH', null=True)
   impact	= models.IntegerField('Amount of impact property could have', null=True)
+  vacancy       = models.IntegerField('Percentile among census tracts of estimated residential vacancy rate, via census data', null=True)
   def __unicode__(self):
     return unicode(self.census_tract)
   class Meta:
