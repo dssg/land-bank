b649d305f417f126579f367c83925ebb24b76e2c
Skyler Whorton <skyler.whorton@gmail.com>
2013-08-12 13:21:00 +0000

    Ooops, checked in an unmerged file

--- web/landbank_data/models/__init__.py
+++ web/landbank_data/models/__init__.py
@@ -7,24 +7,17 @@ from CensusTractCharacteristics import CensusTractCharacteristics
 from CensusTractIncome import CensusTractIncome
 from CensusTractOccupancy import CensusTractOccupancy
 from CensusTract import CensusTract
-<<<<<<< HEAD
-=======
 from CensusBlock import CensusBlock
 from CensusBlockPopulation import CensusBlockPopulation
->>>>>>> 22bade52bf11ce6073f7e77f5a16608467e6392a
 from CmapPlan import CmapPlan
 from CommunityArea import CommunityArea
 from CrimeIncident import CrimeIncident
 from Foreclosure import Foreclosure
 from LoanApplication import LoanApplication
 from Mortgage import Mortgage
-<<<<<<< HEAD
-from PinAreaLookup import PinAreaLookup
-=======
 from Municipality import Municipality
 from PinAreaLookup import PinAreaLookup
 from Population import Population
->>>>>>> 22bade52bf11ce6073f7e77f5a16608467e6392a
 from PropertyTypes import PropertyTypes
 from Scavenger import Scavenger
 from SummaryStats import SummaryStats
@@ -33,8 +26,5 @@ from Transaction import Transaction
 from Vacancy311 import Vacancy311
 from VacancyUSPS import VacancyUSPS
 from Ward import Ward
-<<<<<<< HEAD
-=======
 from CensusTractMapping import CensusTractMapping
 from IndicatorCache import IndicatorCache
->>>>>>> 22bade52bf11ce6073f7e77f5a16608467e6392a
