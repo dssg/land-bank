e41dcc7e7ce3f74f9cdea3cf2d3619cab71d6a7b
tplagge <tplagge@gmail.com>
2013-08-12 05:36:15 +0000

    Refactor indicator cacheing.

--- web/landbank_data/analysis/cache_indicators.py
+++ web/landbank_data/analysis/cache_indicators.py
@@ -54,14 +54,45 @@ def get_tracts_and_weights(\
   
 def get_population(
   tracts=None, communityareas=None,\
-  municipalities=None, wards=None):
+  municipalities=None, wards=None, geoms=None):
+  if geoms is not None:
+    for g in iterable(geoms):
+      if isinstance(g,CensusTract):
+        if tracts==None: tracts=[g]
+        else: tracts.append(g)
+      if isinstance(g,Ward):
+        if wards==None: wards=[g]
+        else: wards.append(g)
+      if isinstance(g,CommunityArea):
+        if communityareas==None: communityareas=[g]
+        else: communityareas.append(g)
+      if isinstance(g,Municipality):
+        if municipalities==None: municipalities=[g]
+        else: municipalities.append(g)
+  tracts = get_tracts_and_weights(tracts,communityareas,municipalities,wards)
+  pops = get_tract_characteristic('pop',tracts.keys())
+  vals = get_tract_characteristic(characteristic,tracts.keys())
   tracts = get_tracts_and_weights(tracts,communityareas,municipalities,wards)
   pops = get_tract_characteristic('pop',tracts.keys())
   return int(sum([pops[i]*tracts[i] for i in pops.keys()]))
 
 def get_pop_weighted_characteristic(characteristic,
   tracts=None, communityareas=None,\
-  municipalities=None, wards=None):
+  municipalities=None, wards=None, geoms=None):
+  if geoms is not None:
+    for g in iterable(geoms):
+      if isinstance(g,CensusTract):
+        if tracts==None: tracts=[g]
+        else: tracts.append(g)
+      if isinstance(g,Ward):
+        if wards==None: wards=[g]
+        else: wards.append(g)
+      if isinstance(g,CommunityArea):
+        if communityareas==None: communityareas=[g]
+        else: communityareas.append(g)
+      if isinstance(g,Municipality):
+        if municipalities==None: municipalities=[g]
+        else: municipalities.append(g)
   tracts = get_tracts_and_weights(tracts,communityareas,municipalities,wards)
   pops = get_tract_characteristic('pop',tracts.keys())
   vals = get_tract_characteristic(characteristic,tracts.keys())
@@ -70,74 +101,68 @@ def get_pop_weighted_characteristic(characteristic,
     if norm > 0 else 0
 
 def cache_census_indicators():
-  for tract in CensusTract.objects.all():
-    cv = IndicatorCache(\
-      area_type='Census Tract', area_id = tract.id,\
-      indicator_name = 'pop',\
-      indicator_value = get_population(tracts=tract))
-    cv.save()
-  for municipality in Municipality.objects.all():
-    cv = IndicatorCache(\
-      area_type='Municipality', area_id = municipality.id,\
-      indicator_name = 'pop',\
-      indicator_value = get_population(municipalities=municipality))
-    cv.save()
-  for communityarea in CommunityArea.objects.all():
-    cv = IndicatorCache(\
-      area_type='Community Area', area_id = communityarea.id,\
-      indicator_name = 'pop',\
-      indicator_value = get_population(communityareas=communityarea))
-    cv.save()
-  for ward in Ward.objects.all():
-    cv = IndicatorCache(\
-      area_type='Ward', area_id = ward.id,\
-      indicator_name = 'pop',\
-      indicator_value = get_population(wards=ward))
-    cv.save()
+  # First, compute the segregation index.
+  tot_pct_white = get_pop_weighted_characteristic("pct_whitenh",\
+    tracts=CensusTract.objects.all())
+  tot_pct_black = get_pop_weighted_characteristic("pct_blacknh",\
+    tracts=CensusTract.objects.all())
+  tot_pct_hisp = get_pop_weighted_characteristic("pct_hispanic",\
+    tracts=CensusTract.objects.all())
+  tot_pct_asian = get_pop_weighted_characteristic("pct_asiannh",\
+    tracts=CensusTract.objects.all())
 
+  # We'll define it as the percentage of people who would have to
+  # move in order for the geometry to match Cook County as a whole.
+  for geom_type,geom_str in \
+    zip([CensusTract,Municipality,Ward,CommunityArea],\
+        ['Census Tract', 'Municipality', 'Ward', 'Community Area']):
+    for geom in geom_type.objects.all():
+      pct_white = get_pop_weighted_characteristic("pct_whitenh",\
+        geoms=geom)
+      pct_black = get_pop_weighted_characteristic("pct_blacknh",\
+        geoms=geom)
+      pct_hisp = get_pop_weighted_characteristic("pct_hispanic",\
+        geoms=geom)
+      pct_asian = get_pop_weighted_characteristic("pct_asiannh",\
+        geoms=geom)
+      segregation = (\
+        abs(tot_pct_white-pct_white)+\
+        abs(tot_pct_black-pct_black)+\
+        abs(tot_pct_hisp-pct_hisp)+\
+        abs(tot_pct_asian-pct_asian))/2.0
+      cv = IndicatorCache(\
+        area_type=geom_str, area_id = geom.id,\
+        indicator_name = 'segregation',\
+        indicator_value = segregation)
+      cv.save()
+    
+  # Now do total population.
+  for geom_type,geom_str in \
+    zip([CensusTract,Municipality,Ward,CommunityArea],\
+        ['Census Tract', 'Municipality', 'Ward', 'Community Area']):
+    for geom in geom_type.objects.all():
+      cv = IndicatorCache(\
+        area_type=geom_str, area_id = geom.id,\
+        indicator_name = 'pop',\
+        indicator_value = get_population(geoms=geom))
+      cv.save()
+
+  # Now a bunch of other indicators.
   for indicator in ['median_age', 'pct_18plus', 'pct_65plus', 'pct_whitenh',\
                     'pct_blacknh', 'pct_asiannh', 'pct_hispanic', 'pct_owner_occupied',\
                     'pct_renter_occupied', 'owner_occ_hh_size', 'renter_occ_hh_size']:
 
-    for tract in CensusTract.objects.all():
-      retval = get_pop_weighted_characteristic(indicator,\
-        tracts=tract)
-      cv = IndicatorCache(\
-        area_type='Census Tract',\
-        area_id  = tract.id,\
-        indicator_name  = indicator,\
-        indicator_value = retval)
-      cv.save()
-
-    for municipality in Municipality.objects.all():
-      retval = get_pop_weighted_characteristic(indicator,\
-        municipalities=municipality)
-      cv = IndicatorCache(\
-        area_type='Municipality',\
-        area_id  = municipality.id,\
-        indicator_name  = indicator,\
-        indicator_value = retval)
-      cv.save()
-
-    for communityarea in CommunityArea.objects.all():
-      retval = get_pop_weighted_characteristic(indicator,\
-        communityareas=communityarea)
-      cv = IndicatorCache(\
-        area_type='Community Area',\
-        area_id  = communityarea.id,\
-        indicator_name  = indicator,\
-        indicator_value = retval)
-      cv.save()
-
-    for ward in Ward.objects.all():
-      retval = get_pop_weighted_characteristic(indicator,\
-        wards=ward)
-      cv = IndicatorCache(\
-        area_type='Ward',\
-        area_id  = ward.id,\
-        indicator_name  = indicator,\
-        indicator_value = retval)
-      cv.save()
+    for geom_type,geom_str in \
+      zip([CensusTract,Municipality,Ward,CommunityArea],\
+          ['Census Tract', 'Municipality', 'Ward', 'Community Area']):
+      for geom in geom_type.objects.all():
+        retval = get_pop_weighted_characteristic(indicator, geoms=geom)
+        cv = IndicatorCache(\
+          area_type=geom_str,\
+          area_id  = geom.id,\
+          indicator_name  = indicator,\
+          indicator_value = retval)
+        cv.save()
 
 def cache_indicators():
   IndicatorCache.objects.all().delete()
