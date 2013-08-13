c1640fe2be608055137639e73b728691d074b56f
Skyler Whorton <skyler.whorton@gmail.com>
2013-08-13 20:56:23 +0000

    Further progress on bootstrap typeahead + google maps autocomplete and added redirect to PIN page from map click, closes #65 as proof-of-concept.

---
+++ web/landbank_data/ajax.py
@@ -0,0 +1,27 @@
+import random
+from dajax.core import Dajax
+from dajaxice.decorators import dajaxice_register
+from django.db import connection
+from models import Assessor
+from django.core import serializers
+
+@dajaxice_register
+def randomize(request):
+    dajax = Dajax()
+    dajax.assign('#result', 'value', random.randint(1,10))
+    dajax.add_data('foo', 'randomize_callback')
+    return dajax.json()
+
+@dajaxice_register
+def get_property_from_latlng(request, lat, lng):
+    lat = str(lat)
+    lng = str(lng)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM latlng_to_pin14(" + lat + ", " + lng + ");")
+    fetched_pin = cursor.fetchone()[0]
+    try: data = serializers.serialize('json', Assessor.objects.filter(pin__exact=fetched_pin)) 
+    except: data = None
+    dajax = Dajax()
+    #dajax.add_data(data, 'pin_callback')
+    dajax.redirect('/pin/'+fetched_pin, delay=0)
+    return dajax.json()
