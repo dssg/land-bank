ca59a64560c6b632a0ff0198d8c0c811e24fed5a
tplagge <tplagge@gmail.com>
2013-08-15 18:09:02 +0000

    Add indices to indicator cache

--- web/landbank_data/models/IndicatorCache.py
+++ web/landbank_data/models/IndicatorCache.py
@@ -1,11 +1,12 @@
 from django.contrib.gis.db import models
 
 class IndicatorCache(models.Model):
-  area_type	= models.CharField('municipality, ward, tract or community area',max_length=100)
-  area_id	= models.IntegerField('Foreign key to the ward, censustract or communityarea table depending on area_type', null=True)
-  indicator_name = models.CharField('indicator name', max_length=100)
+  area_type	= models.CharField('municipality, ward, tract or community area',\
+    max_length=100, db_index=True)
+  area_id	= models.IntegerField('Foreign key to the ward, censustract or communityarea table depending on area_type', null=True, db_index=True)
+  indicator_name = models.CharField('indicator name', max_length=100, db_index=True)
   indicator_value = models.FloatField('indicator value')
-  indicator_date = models.DateTimeField('date of indicator', null=True)
+  indicator_date = models.DateTimeField('date of indicator', null=True, db_index=True)
   def __unicode__(self):
     return unicode(self.area_type) + u' id:' + unicode(self.area_id)
   class Meta:
