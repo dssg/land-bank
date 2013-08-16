2408f204a285c47b16f0857a28d34e2ef449079c
Skyler Whorton <skyler.whorton@gmail.com>
2013-08-16 09:08:25 +0000

    Added color assignment to each geographical area and refined leaflet page

--- web/landbank_data/models/CensusTract.py
+++ web/landbank_data/models/CensusTract.py
@@ -3,6 +3,7 @@ from django.contrib.gis.db import models
 class CensusTract(models.Model):
   fips 				= models.BigIntegerField('Census tract identifying FIPS number', null=True)
   loc                           = models.MultiPolygonField(null=False, srid=3435)
+  color_id                      = models.IntegerField('Map coloring index', null=True)
   objects                       = models.GeoManager()
   def __unicode__(self):
     return unicode(self.fips)
--- web/landbank_data/models/Municipality.py
+++ web/landbank_data/models/Municipality.py
@@ -3,6 +3,7 @@ from django.contrib.gis.db import models
 class Municipality(models.Model):
   name		= models.CharField('Community area name', max_length=80, null=True)
   geom		= models.MultiPolygonField(null=False, srid=3435)
+  color_id      = models.IntegerField('Map coloring index', null=True)
   objects	= models.GeoManager()
   def __unicode__(self):
     return unicode(self.name)
