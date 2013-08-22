243855e85650cd35aedffef471f4a4faaa5b4a8a
Evan Misshula <emisshula@jjay.cuny.edu>
2013-08-22 16:22:04 +0000

    comments in the d3 charts

--- web/landbank_data/static/landbank_data/js/d3plots.js
+++ web/landbank_data/static/landbank_data/js/d3plots.js
@@ -7,13 +7,16 @@ function d3hist(div, data, title, label, marker) {
   data = data.sort(function(a,b) { if (a.x > b.x) return 1; if (a.x < b.x) return -1; return 0; });
   var xdeltas = [];
   for(var i=1; i<data.length; i++) { data[i].xdelta = data[i].x - data[i-1].x; };
+    // creates 10% of distribution bins
   data[0].xdelta = data[1].xdelta;
   var xmin = d3.min(data, function(d) { return d.x; });
   var xmax = d3.max(data, function(d) { return d.x; });
   var ymax = d3.max(data, function(d) { return d.y; });
   var padding = data[0].xdelta * 0.1;
 
-  var xScale = d3.scale.linear()
+    //creates a domain buffer of 10% on each side of the min and max
+    //creates scales and axes
+    var xScale = d3.scale.linear()
       .domain([xmin-data[0].xdelta, xmax+data[data.length-1].xdelta])
       .range([0, width]);
   
@@ -26,6 +29,7 @@ function d3hist(div, data, title, label, marker) {
       .orient("bottom")
       .ticks(5)
       .tickFormat(d3.format("3.0f"));
+
   if (data[0].xdelta < 1.0) {
     xAxis.tickFormat(d3.format("3.1f"));
   }
@@ -36,7 +40,7 @@ function d3hist(div, data, title, label, marker) {
   var yAxis = d3.svg.axis()
       .scale(yScale)
       .orient("left");
-  
+//  add the chart to the computed html
   var svg = d3.select(div).append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
@@ -44,13 +48,14 @@ function d3hist(div, data, title, label, marker) {
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
       .attr("id","chart");
  
+// y axis text
   svg.append("g")
      .append("text")
      .attr("y", -10)
      .attr("x", width/2)
      .style("text-anchor","middle")
      .text(title);
-  
+// x axis text  
   svg.append("g")
       .attr("class", "axis")
       .attr("transform", "translate(0," + height + ")")
@@ -60,7 +65,7 @@ function d3hist(div, data, title, label, marker) {
       .attr("x",width/2)
       .style("text-anchor", "middle")
       .text(label) ;
-  
+  // creates y axis
   svg.append("g")
       .attr("class", "axis")
       .attr("id", "yaxis")
@@ -83,6 +88,7 @@ function d3hist(div, data, title, label, marker) {
         "stroke-width": 3, "stroke": "black"
       });
   };
+    // manually creates y bar
   svg.selectAll(".bar")
       .data(data)
       .enter()
@@ -102,6 +108,7 @@ function d3timeline(div, data, title, marker) {
       width = 320 - margin.left - margin.right,
       height = 320 - margin.top - margin.bottom;
 
+    // need to parse the date and dates from python must be passed as strings
   var parseDate = d3.time.format("%Y-%m-%d").parse;
   
   data.forEach(function(d) { d.x = parseDate(d.x); });
@@ -113,6 +120,8 @@ function d3timeline(div, data, title, marker) {
   var ymax = d3.max(data, function(d) { return d.y; });
   var ybot = d3.max([0,ymin-0.1*(ymax-ymin)]);
   var ytop = ymax+0.1*(ymax-ymin);
+
+    // scales and axes
 
   var xScale = d3.time.scale()
       .domain(d3.extent(data, function(d) { return d.x; }))
@@ -130,17 +139,20 @@ function d3timeline(div, data, title, marker) {
       .scale(yScale)
       .orient("left");
 
+    // computing the line that travels to each data point
   var line = d3.svg.line()
       .interpolate("linear")
       .x(function(d) { return xScale(d.x); })
       .y(function(d) { return yScale(d.y); });
 
+    //append the chart
   var svg = d3.select(div).append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   
+    //append the x axis
   svg.append("g")
       .attr("class", "axis")
       .attr("transform", "translate(0," + height + ")")
@@ -151,6 +163,7 @@ function d3timeline(div, data, title, marker) {
       .style("text-anchor", "middle")
       .text("Time") ;
   
+    //append the y axis
   svg.append("g")
       .attr("class", "axis")
       .call(yAxis)
@@ -171,12 +184,12 @@ function d3timeline(div, data, title, marker) {
       });
   };
 
-
+//append a timeseries class which contains the line
   var timeseries = svg.selectAll(".timeseries")
       .data(data)
       .enter().append("g")
       .attr("class","timeseries");
-
+//append the actual path
   timeseries.append("path")
       .attr("class","line")
       .attr("d", line(data))
